776b6e0bbee131b5918e2af2a1785340059d7684
sangeethamsudheer <sudheerss928@gmail.com>
2025-06-18 15:37:30 +0530

    18

---
+++ line_counter.py
@@ -0,0 +1,15 @@
+def count_lines_of_code(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            return len(lines)
+    except FileNotFoundError:
+        return "File not found. Please check the path."
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+
+if __name__ == "__main__":
+    path = input("Enter the file path: ")
+    result = count_lines_of_code(path)
+    print(f"Number of lines in the file: {result}")
