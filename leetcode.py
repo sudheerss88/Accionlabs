18b68149aad7983b500a392f0c93dd0cbcd060b2
sangeethamsudheer <sudheerss928@gmail.com>
2025-06-18 15:33:56 +0530

    18

--- leetcode.py
+++ leetcode.py
@@ -10,39 +10,3 @@ if __name__ == '__main__':
             print("not weird")
 
 
-def mediansortedarray(self, num1, num2):
-    merged = sorted(num1+num2)
-    sorted = len(merged)
-    if n%2 == 0:
-        return (merged[n//2 - 1] + merged[n//2])/2
-    else:
-        return merged[n//2]
-
-class Solution:
-    def addTwoNumbers(self, l1, l2):
-        dummy = curr = ListNode()
-        carry = 0
-        while l1 or l2 or carry:
-            carry += (l1.val if l1 else 0) + (l2.val if l2 else 0)
-            curr.next = ListNode(carry % 10)
-            carry //= 10
-            curr = curr.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return dummy.next
-
-
-
-
-class Solution:
-
-    def twoSum(self, nums: List[int], target: int) ->List[int]:
-         for i in range(len(nums)):
-            for j in range (i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-
-nums = [2,7,11,15]
-target = [9]
-result =Solution().twoSum(nums,target)
-print(result)
