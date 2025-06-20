291fef7ae75d526efa97c9c8f281b63383c42b5d
sangeethamsudheer <sudheerss928@gmail.com>
2025-06-20 10:50:03 +0530

    19

--- leetcode.py
+++ leetcode.py
@@ -1,12 +1,21 @@
-if __name__ == '__main__':
-        n = int(input().strip())
-        if n % 2 != 0:
-            print("weird")
-        elif n in range(2, 5):
-            print("not weird")
-        elif n in range(6, 20):
-            print("weird")
-        else:
-            print("not weird")
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        total = 0
+        prev = 0
+        for I in reversed(s):
+            val = roman[I]
+            if val<prev:
+                total-=val
+            else:
+                total+=val
+                prev=val
+        return total
+s = Solution()
+print(s.romanToInt("III"))
+print(s.romanToInt("LVIII"))
+print(s.romanToInt("MCMXCIV"))
+
+
 
 
